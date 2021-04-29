d4602a6bec1b7dfc27b7152521061f1429257f82
Joseph Sherman <shermanator2018@gmail.com>
2021-04-29 15:28:00 -0500

    created users app and stuff

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my_apps
     'pizza',
-
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pizzeria/urls.py
+++ pizzeria/urls.py
@@ -17,10 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls)]
-
-''',
+    path('admin/', admin.site.urls),
     path('', include('pizza.urls')),
     path('users/', include('users.urls')),
 ]
-'''
